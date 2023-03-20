9f249023bdff2e0dce77c915f0387b9123960156
melvbouv <melvin.bouvier@isen-ouest.yncrea.fr>
2023-03-20 11:04:01 +0100

    modified:   visualisationC.c

--- visualisationC.c
+++ visualisationC.c
@@ -4,7 +4,7 @@
 #include "visualisationC.h"
 
 
-   void visualisationC(float puissance_f) // Cette fonction permet de récupérer la consigne, elle est affectée par le verrou et elle write true/false dans le fichier data
+   void visualisationC(float puissance) // Cette fonction permet de récupérer la consigne, elle est affectée par le verrou et elle write true/false dans le fichier data
    {
             	
    
